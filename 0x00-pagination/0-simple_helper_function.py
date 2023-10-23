1a66892813d7b9981ede8a39722deea7c7bbe6e6
JEPTUI <alicejeptui@gail.com>
2023-10-23 11:40:18 +0000

    Simple helper function

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Defines a function index_range"""
+
+
+def index_range(page, page_size):
+    if page <= 0 or page_size <= 0:
+        return None
+
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+
+    return start_index, end_index
