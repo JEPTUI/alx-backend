8aea55eb5053cb3178308d2b7fc47949c253d4b3
JEPTUI <alicejeptui@gail.com>
2023-10-24 06:31:06 +0000

    Hypermedia pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -7,7 +7,12 @@ import csv
 import math
 from typing import List
 
+
 def index_range(page, page_size):
+    """Takes two integer arguments page and page_size
+    and returns a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list for those
+    particular pagination parameters"""
     if page <= 0 or page_size <= 0:
         return None
 
@@ -24,46 +29,51 @@ class Server:
 
     def __init__(self):
         self.__dataset = None
-
+    
     def dataset(self) -> List[List]:
         """Cached dataset
-        """
+	"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
-	    self.__dataset = dataset[1:]
+            self.__dataset = dataset[1:]
 
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            """Takes two integer arguments page with default
-            value 1 and page_size with default value 10"""
-            if not (isinstance(page, int) and isinstance(
-                page_size, int) and  page > 0 and page_size > 0):
-                return []
-            start, end = index_range(page, page_size)
-            dataset = self.dataset()
+        """Takes two integer arguments page with default
+        value 1 and page_size with default value 10"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
-            if start >= len(dataset):
-                return []
-
-            return dataset[start:end]
+        start_index, end_index = index_range(page, page_size)
+        data = self.dataset()
+        if start_index >= len(data):
+            return []
+        return data[start_index:end_index]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
-        """Takes the same arguments (and defaults) as
-        get_page and returns a dictionary"""
+        """Takes the same arguments (and defaults) as get_page
+        and returns returns a dictionary containing the following
+        key-value pairs:
+            page_size: the length of the returned dataset page
+            page: the current page number
+            data: the dataset page (equivalent to return from previous task)
+            next_page: number of the next page, None if no next page
+            prev_page: number of the previous page, None if no previous page
+            total_pages: the total number of pages in the dataset as an integer
+        """
         page_data = self.get_page(page, page_size)
         total_pages = math.ceil(len(self.dataset()) / page_size)
-
         next_page = page + 1 if page < total_pages else None
         prev_page = page - 1 if page > 1 else None
 
         return {
-            "page_size": len(page_data),
-            "page": page,
-            "data": page_data,
-            "next_page": next_page,
-            "prev_page": prev_page,
-            "total_pages": total_pages
-        }
+                "page_size": len(page_data),
+                "page": page,
+                "data": page_data,
+                "next_page": next_page,
+                "prev_page": prev_page,
+                "total_pages": total_pages
+                }
