ab51a3804dd0c5c1cceb28bf89913445083fa404
JEPTUI <alicejeptui@gail.com>
2023-10-24 02:29:42 +0000

    Simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,6 +3,9 @@
 
 
 def index_range(page, page_size):
+    """Returns a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list for those
+    particular pagination parameters."""
     if page <= 0 or page_size <= 0:
         return None
 
